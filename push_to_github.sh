b18ad55d4127e05c73beba169535f4ff6ddef5ee
Ubuntu <ubuntu@ip-172-31-37-33.eu-west-1.compute.internal>
2023-08-20 23:37:02 +0000

    Automatic commit at 2023-08-20 23:37:01

--- push_to_github.sh
+++ push_to_github.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 cd /home/ubuntu/exam_k8s
+current_time=$(date +"%Y-%m-%d %H:%M:%S")
+commit_message="Automatic commit at ${current_time}"
 git pull origin main
 git add .
-git commit -m "Automatic commit"
+git commit -m "${commit_message}"
 git push 

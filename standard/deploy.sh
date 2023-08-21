a3e24a700384f35bff6d59d04daf2566df139c17
Ubuntu <ubuntu@ip-172-31-37-33.eu-west-1.compute.internal>
2023-08-21 13:00:03 +0000

    Automatic commit at 2023-08-21 13:00:01

--- standard/deploy.sh
+++ standard/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kubectl apply -f fastapi.yaml -f hpa.yaml -f mysql.yaml -f phpmyadmin.yaml --namespace=standard
+kubectl apply -f fastapi.yaml --namespace=standard -f hpa.yaml --namespace=standard -f mysql.yaml --namespace=standard -f phpmyadmin.yaml --namespace=standard

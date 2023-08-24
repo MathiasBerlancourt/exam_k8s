d6b153987e707063dff923dca094fd999bea0103
Ubuntu <ubuntu@ip-172-31-37-33.eu-west-1.compute.internal>
2023-08-24 11:00:03 +0000

    Automatic commit at 2023-08-24 11:00:01

---
+++ helm/cleanup_namespace.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+NAMESPACE="standard"
+
+# Tableau contenant les types de ressources à supprimer
+RESOURCE_TYPES=("deployment" "statefulset" "service" "pod" "ingress")
+
+# Boucle pour supprimer chaque type de ressource
+for RESOURCE_TYPE in "${RESOURCE_TYPES[@]}"; do
+    kubectl delete "$RESOURCE_TYPE" --all -n "$NAMESPACE"
+done
+
+echo "Cleanup completed in namespace $NAMESPACE."
+
